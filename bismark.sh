6276156fe465838c0a4fb35aae6a2afacb8ddf33
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-09-06 12:59:46 -0500

    Create bismark.sh

---
+++ bismark.sh
@@ -0,0 +1,10 @@
+for i in SRR10070129 SRR10070128 SRR10070127 SRR10070126
+do
+fastq-dump -X 10000 -I --split-files $i
+done
+
+cd /gpfs/home/guosa/hpc/methylation/pancrease/WGBS
+for i in SRR10070129 SRR10070128 SRR10070127 SRR10070126
+do
+bismark --bowtie2 --phred33-quals --multicore 1 --fastq -L 32 -N 0 -D 5 -R 1 ~/hpc/db/hg19/bismark -1 $i\_1.fastq -2 $i\_2.fastq -o ./
+done
