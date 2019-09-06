79b45fdae67fab38afd9332ea883a2a18a08f7b3
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-09-06 13:58:13 -0500

    Update bismark.sh

--- bismark.sh
+++ bismark.sh
@@ -8,3 +8,7 @@ for i in SRR10070129 SRR10070128 SRR10070127 SRR10070126
 do
 bismark --bowtie2 --phred33-quals --multicore 1 --fastq -L 32 -N 0 -D 5 -R 1 ~/hpc/db/hg19/bismark -1 $i\_1.fastq -2 $i\_2.fastq -o ./
 done
+
+grep 'C methylated in CpG context' *.txt
+grep 'C methylated in CHG context' *.txt
+grep 'C methylated in CHH context' *.txt
